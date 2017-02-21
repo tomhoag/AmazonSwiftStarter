3133c10e2eafc13485d8de503d03ccd03fe66755
Tom <tomhoag@Gmail.com>
2017-02-21 16:33:45 -0500

    clean up code

--- AmazonSwiftStarter/AMZRemoteService.swift
+++ AmazonSwiftStarter/AMZRemoteService.swift
@@ -75,32 +75,6 @@ class AMZRemoteService {
     }
     
     // This is where the saving to S3 (image) and DynamoDB (data) is done.
-    func XXsaveAMZUser(_ user: AMZUser, completion: @escaping ErrorResultBlock)  {
-        precondition(user.userId != nil, "You should provide a user object with a userId when saving a user")
-        
-        let mapper = AWSDynamoDBObjectMapper.default()
-        // We create a task that will save the user to DynamoDB
-        // This works because AMZUser extends AWSDynamoDBObjectModel and conforms to AWSDynamoDBModeling
-        let saveToDynamoDBTask: AWSTask = mapper.save(user)
-        
-        // If there is no imageData we only have to save to DynamoDB
-        if user.imageData == nil {
-            saveToDynamoDBTask.continueWith(block: { (task) -> AnyObject? in
-                completion(task.error as NSError?)
-                return nil
-            })
-        } else {
-            // We have to save data to DynamoDB, and the image to S3
-            saveToDynamoDBTask.continueOnSuccessWith(block: { (task) -> AnyObject? in
-                // An example of the AWSTask api. We return a task and continueWithBlock is called on this task.
-                return self.createUploadImageTask(user)
-            }).continueWith(block: { (task) -> AnyObject? in
-                completion(task.error as NSError?)
-                return nil
-            })
-        }
-    }
-    
     func saveAMZUser(_ user: AMZUser, completion: @escaping ErrorResultBlock) {
         precondition(user.userId != nil, "You should provide a user object with a userId when saving a user")
         
@@ -357,12 +331,13 @@ extension AMZRemoteService: RemoteService {
             preconditionFailure("No identity provider available, did you forget to call configure() before using AMZRemoteService?")
         }
         
+        /*
         // nuke any existing identityId
         if identityProvider.identityId != nil {
             identityProvider.clearKeychain()
             assert(identityProvider.identityId == nil)
         }
-        
+        */
         let task: AWSTask = identityProvider.getIdentityId()
         task.continueWith(block: { (task) -> AnyObject? in
             if let error = task.error {
@@ -387,89 +362,7 @@ extension AMZRemoteService: RemoteService {
         
         
     }
-    func oldfetchFacebookUser(_ completion: @escaping UserDataResultBlock) {
-        // This will attempt to fetch a user from the database with the current facebook login
-        precondition(AccessToken.current != nil, "Faceboook token should exist")
-        
-        if(identityProvider == nil) {
-            configure(provider: .facebook)
-        }
-        guard let identityProvider = identityProvider else {
-            preconditionFailure("No identity provider available, did you forget to call configure() before using AMZRemoteService?")
-        }
-        
-        // nuke any existing identityId
-        if identityProvider.identityId != nil {
-            identityProvider.clearKeychain()
-            assert(identityProvider.identityId == nil)
-        }
-        
-        let task: AWSTask = identityProvider.getIdentityId()
-        task.continueWith(block: { (task) -> AnyObject? in
-            if let error = task.error {
-                print("there was a problem getting with identityProvider!.getIdentityId()")
-                completion(nil, error as NSError?)
-            } else {
-                // Mock up the facebook user by assuming that it does exist and attempt to fetch
-                
-                // The identityId was found, this user is authorized via fb login.  Now create a legit user
-                self.persistentUserId = self.identityProvider?.identityId
-                
-                /*
-                 1. Attempt to fetch the current user based on the persistentUserId
-                 2. If there is no userData found and there is no error, create a new user from facebook info
-                 3. To create a new user, nil out the persistentId first!!
-                 */
-                self.fetchCurrentUser({ (userData, error) in
-                    
-                    if(error == nil && userData == nil) { // add the fb data to the user
-                        
-                        self.currentUser = nil // nil these out so createUser doesn't barf.  cU will refetch the identityId
-                        self.persistentUserId = nil
-                        
-                        // get the fb userData to pass into createUser
-                        var facebookUserData = UserDataValue()
-                        facebookUserData.name = UserProfile.current?.fullName
-                        
-                        // Create and run a URLSession to fetch the fb profile pic
-                        let fbID = UserProfile.current?.userId
-                        let profileURL = URL(string:"https://graph.facebook.com/"+fbID!+"/picture?type=large")
-                        let session = URLSession(configuration: .default)
-                        let downloadPicTask = session.dataTask(with: profileURL!, completionHandler: { (data, response, error) in
-                            if let error = error {
-                                // no biggie, maybe there isn't a profile pic??
-                                print(">>>>>> error fetching fb profile pic: \(error)")
-                            } else {
-                                if let data = data {
-                                    facebookUserData.imageData = data
-                                }
-                            }
-                            self.createCurrentUser(facebookUserData, completion: { (error) -> Void in
-                                print(">>>>>>> Finished creating new user from FB profile!!")
-                                completion(facebookUserData, error)
-                            })
-                        })
-                        downloadPicTask.resume()
-                        
-                    } else { // userData or error was not nil
-                        if(userData != nil) {
-                            //self.currentUser = nil
-                            //self.persistentUserId = nil
-                            completion(userData, error)
-                        } else {
-                            self.currentUser = nil
-                            self.persistentUserId = nil
-                            completion(userData, error)
-                        }
-                    }
-                    
-                })
-            }
-            return nil
-        })
-    }
 
-    
     // MARK: - Additional configure()-ers
     func configure() {
         configure(provider: .anonymous)
